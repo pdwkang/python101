ffa6710893b66a5da6d8cd9d792e4b8955000ebf
Paul <dwkang@uga.edu>
2017-02-20 10:18:29 -0500

    first commit

---
+++ the_basics.py
@@ -0,0 +1,30 @@
+# print "Paul Kang"
+
+# animals = ['wolf','giraffe','hippo']
+# animals[1] = 'asefwef'
+# print animals
+
+dc_class = "Michael, Paul, Mason, Casey, Connie"
+dc_class_as_array = dc_class.split(',')
+# print dc_class
+# print dc_class_as_array
+# print sorted(dc_class_as_array)
+
+# reverse method - reverses the array, not on value, but on index
+# dc_class_as_array.reverse();
+# print dc_class_as_array
+
+# len attribute. it works like .length in JS
+# print len(dc_class_as_array)
+
+# creates a copy of the array
+# print dc_class_as_array[2:len(dc_class_as_array)]
+
+# for loop is [for] [what_you_call_this_one] [var] [in] 
+# for students in dc_class_as_array: print students
+	# Indentation matters in python
+# for i in range(1,10) : print dc_class
+
+# a function in python is not a function. it is a definition, ergo, def
+def sayHello():print "hello"
+sayHello()
